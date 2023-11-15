16798642b56d77ce061dbfd0e5008f2f29c7c99e
Santiago <santiagocortes996@hotmail.com>
2023-11-15 09:41:28 -0300

    ws host

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io("ws://localhost:3500");
+const socket = io("ws://chatroom-c01r.onrender.com/");
 
 const msgInput = document.querySelector("#message");
 const nameInput = document.querySelector("#name");
