9ee44128acc7a669350a0d9f471d949389f53818
Santiago <santiagocortes996@hotmail.com>
2023-11-15 09:36:57 -0300

    first commit

---
+++ public/app.js
@@ -0,0 +1,107 @@
+const socket = io("ws://localhost:3500");
+
+const msgInput = document.querySelector("#message");
+const nameInput = document.querySelector("#name");
+const chatRoom = document.querySelector("#room");
+const activity = document.querySelector(".activity");
+const usersList = document.querySelector(".user-list");
+const roomList = document.querySelector(".room-list");
+const chatDisplay = document.querySelector(".chat-display");
+
+function sendMessage(e) {
+  e.preventDefault();
+  if (nameInput.value && msgInput.value && chatRoom.value) {
+    socket.emit("message", {
+      text: msgInput.value,
+      name: nameInput.value,
+    });
+    msgInput.value = "";
+  }
+  msgInput.focus();
+}
+
+function enterRoom(e) {
+  e.preventDefault();
+  if (nameInput.value && chatRoom.value) {
+    socket.emit("enterRoom", {
+      name: nameInput.value,
+      room: chatRoom.value,
+    });
+  }
+}
+
+document.querySelector(".form-message").addEventListener("submit", sendMessage);
+document.querySelector(".form-join").addEventListener("submit", enterRoom);
+msgInput.addEventListener("keypress", () => {
+  socket.emit("activity", nameInput.value);
+});
+
+// Listen for messages
+socket.on("message", ({ name, text, time }) => {
+  activity.textContent = "";
+  const li = document.createElement("li");
+  li.className = "post";
+  if (name === nameInput.value) li.className = "post post--left";
+  if (name !== nameInput.value && name !== "Admin")
+    li.className = "post post--right";
+  if (name !== "Admin") {
+    li.innerHTML = `<div class="post__header ${
+      name === nameInput.value ? "post__header--user" : "post__header--reply"
+    }">
+      <span class="post__header--name">${name}</span>
+      <span class="post__header--time">${time}</span>
+      </div>
+      <div class="post__text">${text}</div>
+    `;
+  } else {
+    li.innerHTML = `<div class="post__text">${text}</div>`;
+  }
+  document.querySelector(".chat-display").appendChild(li);
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
+});
+
+let activityTimer;
+socket.on("activity", (name) => {
+  activity.textContent = `${name} esta escribiendo...`;
+
+  // Clear after 1 second
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 3000);
+});
+
+socket.on("userList", ({ users }) => {
+  showUsers(users);
+});
+
+socket.on("roomsList", ({ rooms }) => {
+  console.log(rooms)
+  showRooms(rooms);
+});
+
+function showUsers(users) {
+  usersList.textContent = "";
+  if (users) {
+    usersList.innerHTML = `<em>Usuarios en ${chatRoom.value}:</em>`;
+    users.forEach((user, index) => {
+      usersList.textContent += ` ${user.name}`;
+      if (users.length > 1 && index !== users.length - 1) {
+        usersList.textContent += ",";
+      }
+    });
+  }
+}
+
+function showRooms(rooms) {
+  roomList.textContent = "";
+  if (rooms) {
+    roomList.innerHTML = `<em>Salas activas:</em>`;
+    rooms.forEach((room, index) => {
+      roomList.textContent += ` ${room}`;
+      if (rooms.length > 1 && index !== rooms.length - 1) {
+        roomList.textContent += ",";
+      }
+    });
+  }
+}
